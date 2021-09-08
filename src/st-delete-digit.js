578b514154d84f92e6c17318b21dd798dd344d0a
Maxim <kiber987@gmail.com>
2021-09-08 23:53:37 +0300

    feat: st-encode-line.js OK

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+export default function deleteDigit(n) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let digits = n.toString().split('');
+  let arr = digits;
+  let max = 0;
+  // return digits; ['1', '5', '2']
+  return arr; //+digits.splice(i, 1);
+  for (let i of digits){
+    let arrR = digits;
+    return digits.splice(0, 1);
+    // return arrR;
+
+    // let digits = 
+    // arr.push(digits.splice(i, 1));
+    // return arr; //+digits.splice(i, 1);
+
+  }
+  return Math.max(arr);
 }
--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+export default function encodeLine(str) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // return str;
+  // let str = 'aabbbc';
+  let num = 1;
+  let result = []; // [str+' '];//[];
+  for (let i=0; i<str.length; i++){
+    if (str[i+1] == str[i]) num++;
+    else{
+      if (num != 1) result.push(num+str[i]);
+      else result.push(str[i]);
+      num = 1;
+    }
+  }
+  
+  return result.join('');  
 }
+
+// -3a6t
+// +4a4t
